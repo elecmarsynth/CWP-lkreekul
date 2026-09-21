3102f1c52ee7444916d31e648f57d9527d6eb374
elecmarsynth <lun.tonlill16@gmail.com>
2026-09-21 20:31:56 +0700

    ex30
    to25

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+num = int(input())
+if num > 25 :
+    print("Error")
+else :
+    while num <=25 :
+        print (f"Inside the loop, my variable is {num}")
+        num +=1
